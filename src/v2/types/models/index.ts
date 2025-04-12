27a5c8465f1f553a9da4f736b420b64c5841b5e0
Jake Leventhal <jakeleventhal@me.com>
2025-04-11 21:23:54 -0400

    Added warehouses v2 endpoints

--- src/v2/types/models/index.ts
+++ src/v2/types/models/index.ts
@@ -1,5 +1,7 @@
+export * from './Address';
 export * from './Carrier';
 export * from './Error';
 export * from './Manifest';
 export * from './PackageType';
 export * from './Tag';
+export * from './Warehouse';
--- src/v2/types/resources/index.ts
+++ src/v2/types/resources/index.ts
@@ -2,3 +2,4 @@ export * from './carriers';
 export * from './downloads';
 export * from './manifests';
 export * from './tags';
+export * from './warehouses';
