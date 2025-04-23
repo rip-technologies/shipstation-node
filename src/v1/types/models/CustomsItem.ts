5863a38c2b0277a8df7c7cc33cfc896dd631e783
Jake Leventhal <jakeleventhal@me.com>
2025-04-23 16:02:45 -0400

    Fixing types for v1 order international options

--- src/v1/types/models/CustomsItem.ts
+++ src/v1/types/models/CustomsItem.ts
@@ -3,9 +3,8 @@ export interface CustomsItem {
    * When this field is not submitted in the [Order/CreateOrder call](https://www.shipstation.com/docs/api/orders/create-update-order/),
    * it will create a new customs line. If this field is included when submitting an order through Order/CreateOrder,
    * then it will look for the corresponding customs line and update any values.
-   * @readonly
    */
-  customsItemId: number;
+  customsItemId?: number;
   /** A short description of the CustomsItem */
   description: string;
   /** The quantity for this line item */
@@ -13,7 +12,7 @@ export interface CustomsItem {
   /** The value (in USD) of the line item */
   value: number;
   /** The Harmonized Commodity Code for this line item */
-  harmonizedTariffCode: string;
+  harmonizedTariffCode?: string;
   /**
    * The [two-letter ISO Origin Country code](https://www.nationsonline.org/oneworld/country_code_list.htm) where the
    * item originated
